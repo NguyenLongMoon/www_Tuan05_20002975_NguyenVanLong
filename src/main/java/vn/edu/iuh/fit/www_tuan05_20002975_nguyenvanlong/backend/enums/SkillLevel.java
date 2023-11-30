94e89e2655ca05b2afb6f34d869048439ab03641
NguyenLongMoon <longsky0912624119@gmail.com>
2023-12-01 01:22:56 +0700

    cap nhat entities va enums

---
+++ src/main/java/vn/edu/iuh/fit/www_tuan05_20002975_nguyenvanlong/backend/enums/SkillLevel.java
@@ -0,0 +1,8 @@
+package vn.edu.iuh.fit.www_tuan05_20002975_nguyenvanlong.backend.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum SkillLevel {
+    MASTER, BEGINER, ADVANCED, PROFESSIONAL, IMTERMEDIATE;
+}
